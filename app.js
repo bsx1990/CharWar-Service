84b977966b3d1662c09faed4d3ad80f8f0e20a6f
ZhangNing <zhangning885@pingan.com.cn>
2019-07-03 08:55:33 +0800

    增加初始化数据的逻辑

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
 const app = require("http").createServer();
 const IO = require("socket.io")(app);
+const config = require("./config/game-config");
 
-const PORT = 1001;
+let playgroundCards = [];
+let candidateCards = [];
 
-app.listen(PORT);
+app.listen(config.PORT);
+
 IO.on("connection", function(socket) {
-  socket.on("clickCard", function() {
-    console.log("received clickCard request");
+  console.log("received connection request");
+
+  socket.on(config.requestType.getData, function() {
+    console.log("received getData request");
+    initDefaultPlaygroundCards();
+    socket.emit(config.responseType.playgroundCardsChanged, playgroundCards);
+    appendCandidateCard();
+    appendCandidateCard();
+    socket.emit(config.responseType.candidateCardsChanged, candidateCards);
   });
 });
 
-console.log("app listen at" + PORT);
+function initDefaultPlaygroundCards() {
+  for (let row = 0; row < config.PLAYGROUND_SIZE; row++) {
+    playgroundCards[row] = [];
+    for (let column = 0; column < config.PLAYGROUND_SIZE; column++) {
+      playgroundCards[row][column] = null;
+    }
+  }
+}
+
+function appendCandidateCard() {
+  candidateCards.push(generateCard());
+}
+
+function generateCard() {
+  let maxCard = Math.max(...Array.prototype.concat.apply([], playgroundCards));
+  if (isNaN(maxCard)) {
+    maxCard = 0;
+  }
+
+  if (maxCard > config.MAX_GENERATED_CARD) {
+    maxCard = config.MAX_GENERATED_CARD;
+  }
+
+  return Math.trunc(Math.random() * maxCard + 1);
+}
+
+console.log("app listen at:" + config.PORT);
---
+++ config/game-config.js
@@ -0,0 +1,12 @@
+exports.requestType = {
+  getData: "getData"
+};
+
+exports.responseType = {
+  playgroundCardsChanged: "playgroundCardsChanged",
+  candidateCardsChanged: "CandidateCardsChanged"
+};
+
+exports.PORT = 1001;
+exports.PLAYGROUND_SIZE = 4;
+exports.MAX_GENERATED_CARD = 9;
