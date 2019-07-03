281317f55dc5c64955814193d2bbd23ad92dc8d4
ZhangNing <zhangning885@pingan.com.cn>
2019-07-03 09:58:24 +0800

    增加点击卡牌事件的业务处理逻辑

--- app.js
+++ app.js
@@ -4,21 +4,38 @@ const config = require("./config/game-config");
 
 let playgroundCards = [];
 let candidateCards = [];
+let socket;
 
 app.listen(config.PORT);
 
-IO.on("connection", function(socket) {
+IO.on("connection", function(msgResponse) {
   console.log("received connection request");
 
-  socket.on(config.requestType.getData, function() {
+  socket = msgResponse;
+  socket.on(config.requestType.getData, () => {
     console.log("received getData request");
+
     initDefaultPlaygroundCards();
-    socket.emit(config.responseType.playgroundCardsChanged, playgroundCards);
+    emitPlaygroundCardsChanged();
+
     appendCandidateCard();
     appendCandidateCard();
-    socket.emit(config.responseType.candidateCardsChanged, candidateCards);
+    emitCandidateCardsChanged();
+  });
+
+  msgResponse.on(config.requestType.clickCard, (rowIndex, columnIndex) => {
+    console.log(`received clickCard request, rowIndex:${rowIndex} columnIndex:${columnIndex}`);
+    clickCard(rowIndex, columnIndex);
   });
 });
+
+function emitCandidateCardsChanged() {
+  socket.emit(config.responseType.candidateCardsChanged, candidateCards);
+}
+
+function emitPlaygroundCardsChanged() {
+  socket.emit(config.responseType.playgroundCardsChanged, playgroundCards);
+}
 
 function initDefaultPlaygroundCards() {
   for (let row = 0; row < config.PLAYGROUND_SIZE; row++) {
@@ -44,6 +61,70 @@ function generateCard() {
   }
 
   return Math.trunc(Math.random() * maxCard + 1);
+}
+
+function clickCard(rowIndex, columnIndex) {
+  if (playgroundCards[rowIndex][columnIndex] != null) {
+    return;
+  }
+
+  let currentCandidateCard = candidateCards.shift();
+  appendCandidateCard();
+  emitCandidateCardsChanged();
+
+  playgroundCards[rowIndex][columnIndex] = currentCandidateCard;
+  emitPlaygroundCardsChanged();
+
+  let combinedCardsIndexs = getSameCardsFromAround(rowIndex, columnIndex);
+  while (combinedCardsIndexs.length > 0) {
+    combineCards(combinedCardsIndexs, rowIndex, columnIndex);
+    emitPlaygroundCardsChanged();
+    combinedCardsIndexs = getSameCardsFromAround(rowIndex, columnIndex);
+  }
+}
+
+function getSameCardsFromAround(rowIndex, columnIndex) {
+  const centerCard = playgroundCards[rowIndex][columnIndex];
+  let result = [];
+
+  const arrows = [
+    config.ARROW.LEFT_UP,
+    config.ARROW.UP,
+    config.ARROW.RIGHT_UP,
+    config.ARROW.LEFT,
+    config.ARROW.RIGHT,
+    config.ARROW.LEFT_DOWN,
+    config.ARROW.DOWN,
+    config.ARROW.RIGHT_DOWN
+  ];
+  arrows.forEach(arrow => {
+    const columnOffset = arrow[0];
+    const rowOffset = arrow[1];
+    const targetRowIndex = rowIndex + rowOffset;
+    const targetColumnIndex = columnIndex + columnOffset;
+
+    if (isOutofPlaygroundRange(targetRowIndex) || isOutofPlaygroundRange(targetColumnIndex)) {
+      return;
+    }
+
+    if (playgroundCards[targetRowIndex][targetColumnIndex] === centerCard) {
+      result.push([targetRowIndex, targetColumnIndex]);
+    }
+  });
+
+  return result;
+}
+
+function isOutofPlaygroundRange(index) {
+  return config.PLAYGROUND_SIZE <= index || index < 0;
+}
+
+function combineCards(combinedCardsIndexs, rowIndex, columnIndex) {
+  combinedCardsIndexs.forEach(index => {
+    playgroundCards[index[0]][index[1]] = null;
+  });
+
+  playgroundCards[rowIndex][columnIndex] = playgroundCards[rowIndex][columnIndex] + 1;
 }
 
 console.log("app listen at:" + config.PORT);
--- config/game-config.js
+++ config/game-config.js
@@ -1,5 +1,6 @@
 exports.requestType = {
-  getData: "getData"
+  getData: "getData",
+  clickCard: "clickCard"
 };
 
 exports.responseType = {
@@ -10,3 +11,14 @@ exports.responseType = {
 exports.PORT = 1001;
 exports.PLAYGROUND_SIZE = 4;
 exports.MAX_GENERATED_CARD = 9;
+
+exports.ARROW = {
+  LEFT_UP: [-1, -1],
+  UP: [0, -1],
+  RIGHT_UP: [1, -1],
+  LEFT: [-1, 0],
+  RIGHT: [1, 0],
+  LEFT_DOWN: [-1, 1],
+  DOWN: [0, 1],
+  RIGHT_DOWN: [1, 1]
+};
