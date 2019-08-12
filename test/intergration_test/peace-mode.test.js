a0b3638a42d94657085908c706b509954b1bf5b7
ZhangNing <zhangning885@pingan.com.cn>
2019-08-12 09:27:56 +0800

    增加simple combined测试

--- test/intergration_test/peace-mode.test.js
+++ test/intergration_test/peace-mode.test.js
@@ -6,6 +6,7 @@ const GAME_MODE_CHANGED = GAME_SYSTEM.GAME_MODE_CHANGED;
 const GET_DATA = GAME_SYSTEM.GET_DATA;
 const GAME_MODES = GAME_SYSTEM.GAME_MODES;
 const CLICK_CARD = GAME_SYSTEM.CLICK_CARD;
+const REPLAY = GAME_SYSTEM.REPLAY;
 
 const PEACE_MODES_INDEX = 0;
 const TOKEN = 'UnitTest';
@@ -26,6 +27,11 @@ describe('Peace Mode', function() {
       clientSocket.emit(GET_DATA);
       done();
     });
+  });
+
+  afterEach(function(done) {
+    clientSocket.emit(REPLAY);
+    done();
   });
 
   describe('Put First Card', function() {
@@ -53,6 +59,58 @@ describe('Peace Mode', function() {
         ]);
         var expectedPlaygroundCards = [[1, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]];
         var expectedScore = 0;
+        var expectedGameState = '';
+        var expectedCombinedSkills = [];
+
+        var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+        var resultNumberMap = gameDatas.numberCardsMap;
+        var resultCharMap = gameDatas.charCardsMap;
+        var resultEmptyMap = gameDatas.emptyCardsMap;
+        var resultPlaygroundCards = gameDatas.playgroundCards;
+        var resultScore = gameDatas.score;
+        var resultGameState = gameDatas.gameState;
+        var resultCombinedSkills = gameDatas.combinedSkills;
+
+        expectedNumberMap.should.be.deepEqual(resultNumberMap);
+        expectedCharMap.should.be.deepEqual(resultCharMap);
+        expectedEmptyMap.should.be.deepEqual(resultEmptyMap);
+        expectedPlaygroundCards.should.be.deepEqual(resultPlaygroundCards);
+        expectedScore.should.be.deepEqual(resultScore);
+        expectedGameState.should.be.deepEqual(resultGameState);
+        expectedCombinedSkills.should.be.deepEqual(resultCombinedSkills);
+        done();
+      }, 1000);
+    });
+  });
+
+  describe('Simple Combine', function() {
+    it('should have one card in number map, card value should be 2', function(done) {
+      clientSocket.emit(CLICK_CARD, 0, 1);
+      setTimeout(() => {
+        clientSocket.emit(CLICK_CARD, 0, 0);
+      }, 100);
+      setTimeout(() => {
+        var expectedNumberMap = new Map([['0/0', { row: 0, column: 0, key: '0/0', value: 2 }]]);
+        var expectedCharMap = new Map();
+        var expectedEmptyMap = new Map([
+          ['0/1', { row: 0, column: 1, key: '0/1', value: null }],
+          ['0/2', { row: 0, column: 2, key: '0/2', value: null }],
+          ['0/3', { row: 0, column: 3, key: '0/3', value: null }],
+          ['1/0', { row: 1, column: 0, key: '1/0', value: null }],
+          ['1/1', { row: 1, column: 1, key: '1/1', value: null }],
+          ['1/2', { row: 1, column: 2, key: '1/2', value: null }],
+          ['1/3', { row: 1, column: 3, key: '1/3', value: null }],
+          ['2/0', { row: 2, column: 0, key: '2/0', value: null }],
+          ['2/1', { row: 2, column: 1, key: '2/1', value: null }],
+          ['2/2', { row: 2, column: 2, key: '2/2', value: null }],
+          ['2/3', { row: 2, column: 3, key: '2/3', value: null }],
+          ['3/0', { row: 3, column: 0, key: '3/0', value: null }],
+          ['3/1', { row: 3, column: 1, key: '3/1', value: null }],
+          ['3/2', { row: 3, column: 2, key: '3/2', value: null }],
+          ['3/3', { row: 3, column: 3, key: '3/3', value: null }]
+        ]);
+        var expectedPlaygroundCards = [[2, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]];
+        var expectedScore = 1;
         var expectedGameState = '';
         var expectedCombinedSkills = [];
 
