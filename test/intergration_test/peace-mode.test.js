07b89baeb08e866202acb1ffd9b6c65a2ed302a7
ZhangNing <zhangning885@pingan.com.cn>
2019-08-12 15:34:59 +0800

    增加combined two rounds单元测试

--- test/intergration_test/peace-mode.test.js
+++ test/intergration_test/peace-mode.test.js
@@ -8,7 +8,7 @@ const GAME_MODES = GAME_SYSTEM.GAME_MODES;
 const CLICK_CARD = GAME_SYSTEM.CLICK_CARD;
 const REPLAY = GAME_SYSTEM.REPLAY;
 
-const PEACE_MODES_INDEX = 0;
+const PEACE_MODES = 'peace';
 const TOKEN = 'UnitTest';
 const PORT = 2000;
 
@@ -23,7 +23,7 @@ describe('Peace Mode', function() {
   beforeEach(function(done) {
     clientSocket = CLIENT_IO(`ws://192.168.12.65:${PORT}?token=${TOKEN}`);
     clientSocket.on('connect', function() {
-      clientSocket.emit(GAME_MODE_CHANGED, GAME_MODES[PEACE_MODES_INDEX]);
+      clientSocket.emit(GAME_MODE_CHANGED, PEACE_MODES);
       clientSocket.emit(GET_DATA);
       done();
     });
@@ -187,6 +187,81 @@ describe('Peace Mode', function() {
         expectedCombinedSkills.should.be.deepEqual(resultCombinedSkills);
         done();
       }, 1000);
+    });
+  });
+
+  describe('Combine two rounds', function() {
+    it('should have one card in number map, card value should be 3', function(done) {
+      setTimeout(() => {
+        var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+        var mockedNumberMap = new Map([['1/0', { row: 1, column: 0, key: '1/0', value: 1 }], ['1/1', { row: 1, column: 1, key: '1/1', value: 2 }]]);
+        var mockedEmptyMap = new Map([
+          ['0/0', { row: 0, column: 0, key: '0/0', value: null }],
+          ['0/1', { row: 0, column: 1, key: '0/1', value: null }],
+          ['0/2', { row: 0, column: 2, key: '0/2', value: null }],
+          ['0/3', { row: 0, column: 3, key: '0/3', value: null }],
+          ['1/2', { row: 1, column: 2, key: '1/2', value: null }],
+          ['1/3', { row: 1, column: 3, key: '1/3', value: null }],
+          ['2/0', { row: 2, column: 0, key: '2/0', value: null }],
+          ['2/1', { row: 2, column: 1, key: '2/1', value: null }],
+          ['2/2', { row: 2, column: 2, key: '2/2', value: null }],
+          ['2/3', { row: 2, column: 3, key: '2/3', value: null }],
+          ['3/0', { row: 3, column: 0, key: '3/0', value: null }],
+          ['3/1', { row: 3, column: 1, key: '3/1', value: null }],
+          ['3/2', { row: 3, column: 2, key: '3/2', value: null }],
+          ['3/3', { row: 3, column: 3, key: '3/3', value: null }]
+        ]);
+        var mockedPlaygroundCards = [[null, null, null, null], [1, 2, null, null], [null, null, null, null], [null, null, null, null]];
+        var mockedCandidateCards = [1, 1];
+        gameDatas.numberCardsMap = mockedNumberMap;
+        gameDatas.emptyCardsMap = mockedEmptyMap;
+        gameDatas.playgroundCards = mockedPlaygroundCards;
+        gameDatas.candidateCards = mockedCandidateCards;
+
+        clientSocket.emit(CLICK_CARD, 0, 0);
+        setTimeout(() => {
+          var expectedNumberMap = new Map([['0/0', { row: 0, column: 0, key: '0/0', value: 3 }]]);
+          var expectedCharMap = new Map();
+          var expectedEmptyMap = new Map([
+            ['0/1', { row: 0, column: 1, key: '0/1', value: null }],
+            ['0/2', { row: 0, column: 2, key: '0/2', value: null }],
+            ['0/3', { row: 0, column: 3, key: '0/3', value: null }],
+            ['1/0', { row: 1, column: 0, key: '1/0', value: null }],
+            ['1/1', { row: 1, column: 1, key: '1/1', value: null }],
+            ['1/2', { row: 1, column: 2, key: '1/2', value: null }],
+            ['1/3', { row: 1, column: 3, key: '1/3', value: null }],
+            ['2/0', { row: 2, column: 0, key: '2/0', value: null }],
+            ['2/1', { row: 2, column: 1, key: '2/1', value: null }],
+            ['2/2', { row: 2, column: 2, key: '2/2', value: null }],
+            ['2/3', { row: 2, column: 3, key: '2/3', value: null }],
+            ['3/0', { row: 3, column: 0, key: '3/0', value: null }],
+            ['3/1', { row: 3, column: 1, key: '3/1', value: null }],
+            ['3/2', { row: 3, column: 2, key: '3/2', value: null }],
+            ['3/3', { row: 3, column: 3, key: '3/3', value: null }]
+          ]);
+          var expectedPlaygroundCards = [[3, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]];
+          var expectedScore = 3;
+          var expectedGameState = '';
+          var expectedCombinedSkills = [];
+
+          var resultNumberMap = gameDatas.numberCardsMap;
+          var resultCharMap = gameDatas.charCardsMap;
+          var resultEmptyMap = gameDatas.emptyCardsMap;
+          var resultPlaygroundCards = gameDatas.playgroundCards;
+          var resultScore = gameDatas.score;
+          var resultGameState = gameDatas.gameState;
+          var resultCombinedSkills = gameDatas.combinedSkills;
+
+          expectedNumberMap.should.be.deepEqual(resultNumberMap);
+          expectedCharMap.should.be.deepEqual(resultCharMap);
+          expectedEmptyMap.should.be.deepEqual(resultEmptyMap);
+          expectedPlaygroundCards.should.be.deepEqual(resultPlaygroundCards);
+          expectedScore.should.be.deepEqual(resultScore);
+          expectedGameState.should.be.deepEqual(resultGameState);
+          expectedCombinedSkills.should.be.deepEqual(resultCombinedSkills);
+          done();
+        }, 1000);
+      }, 200);
     });
   });
 });
