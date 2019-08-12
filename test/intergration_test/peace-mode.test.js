5daadb5d749ad61c82cb286b54f44c5bc4203e95
ZhangNing <zhangning885@pingan.com.cn>
2019-08-12 13:22:55 +0800

    增加combined two cards 测试

--- test/intergration_test/peace-mode.test.js
+++ test/intergration_test/peace-mode.test.js
@@ -134,4 +134,59 @@ describe('Peace Mode', function() {
       }, 1000);
     });
   });
+
+  describe('Combine Two Cards', function() {
+    it('should have one card in number map, card value should be 2', function(done) {
+      clientSocket.emit(CLICK_CARD, 0, 0);
+      setTimeout(() => {
+        clientSocket.emit(CLICK_CARD, 0, 2);
+      }, 100);
+      setTimeout(() => {
+        clientSocket.emit(CLICK_CARD, 0, 1);
+      }, 100);
+      setTimeout(() => {
+        var expectedNumberMap = new Map([['0/1', { row: 0, column: 1, key: '0/1', value: 2 }]]);
+        var expectedCharMap = new Map();
+        var expectedEmptyMap = new Map([
+          ['0/0', { row: 0, column: 0, key: '0/0', value: null }],
+          ['0/2', { row: 0, column: 2, key: '0/2', value: null }],
+          ['0/3', { row: 0, column: 3, key: '0/3', value: null }],
+          ['1/0', { row: 1, column: 0, key: '1/0', value: null }],
+          ['1/1', { row: 1, column: 1, key: '1/1', value: null }],
+          ['1/2', { row: 1, column: 2, key: '1/2', value: null }],
+          ['1/3', { row: 1, column: 3, key: '1/3', value: null }],
+          ['2/0', { row: 2, column: 0, key: '2/0', value: null }],
+          ['2/1', { row: 2, column: 1, key: '2/1', value: null }],
+          ['2/2', { row: 2, column: 2, key: '2/2', value: null }],
+          ['2/3', { row: 2, column: 3, key: '2/3', value: null }],
+          ['3/0', { row: 3, column: 0, key: '3/0', value: null }],
+          ['3/1', { row: 3, column: 1, key: '3/1', value: null }],
+          ['3/2', { row: 3, column: 2, key: '3/2', value: null }],
+          ['3/3', { row: 3, column: 3, key: '3/3', value: null }]
+        ]);
+        var expectedPlaygroundCards = [[null, 2, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]];
+        var expectedScore = 2;
+        var expectedGameState = '';
+        var expectedCombinedSkills = [];
+
+        var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+        var resultNumberMap = gameDatas.numberCardsMap;
+        var resultCharMap = gameDatas.charCardsMap;
+        var resultEmptyMap = gameDatas.emptyCardsMap;
+        var resultPlaygroundCards = gameDatas.playgroundCards;
+        var resultScore = gameDatas.score;
+        var resultGameState = gameDatas.gameState;
+        var resultCombinedSkills = gameDatas.combinedSkills;
+
+        expectedNumberMap.should.be.deepEqual(resultNumberMap);
+        expectedCharMap.should.be.deepEqual(resultCharMap);
+        expectedEmptyMap.should.be.deepEqual(resultEmptyMap);
+        expectedPlaygroundCards.should.be.deepEqual(resultPlaygroundCards);
+        expectedScore.should.be.deepEqual(resultScore);
+        expectedGameState.should.be.deepEqual(resultGameState);
+        expectedCombinedSkills.should.be.deepEqual(resultCombinedSkills);
+        done();
+      }, 1000);
+    });
+  });
 });
