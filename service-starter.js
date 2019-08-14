ea23b721110cc2904f7f4069180aea99ea640938
ZhangNing <zhangning885@pingan.com.cn>
2019-08-14 15:56:37 +0800

    绕过两个测试同时跑的时候会报错的问题

--- service-starter.js
+++ service-starter.js
@@ -1,5 +1,5 @@
-const APP = require('http').createServer();
-const IO = require('socket.io')(APP);
+let APP = require('http').createServer();
+let IO = require('socket.io')(APP);
 const GAME_SYSTEM = require('./game_system/game-system');
 
 const GET_DATA = GAME_SYSTEM.GET_DATA;
@@ -9,8 +9,10 @@ const GAME_MODE_CHANGED = GAME_SYSTEM.GAME_MODE_CHANGED;
 const recordInfor = GAME_SYSTEM.recordInfor;
 const recordError = GAME_SYSTEM.recordError;
 
-function setSubscribeForSocketIO(io) {
-  io.on('connection', function(socket) {
+function startServer(port) {
+  APP.listen(port);
+  IO = require('socket.io')(APP);
+  IO.on('connection', function(socket) {
     let token = GAME_SYSTEM.getTokenBySocket(socket);
     recordInfor(`received connection request, request id: ${socket.id}, token is:${token}`);
     if (token == undefined || token == null) {
@@ -18,45 +20,54 @@ function setSubscribeForSocketIO(io) {
       return;
     }
 
-    socket.on(GET_DATA, () => {
-      const token = GAME_SYSTEM.getTokenBySocket(socket);
-      const gameDatas = GAME_SYSTEM.getGameDatasByToken(token);
-      recordInfor(`received getData request, request id: ${socket.id}, token is:${token}`);
-      GAME_SYSTEM.emitGameDatas(gameDatas);
-    });
-
-    socket.on(CLICK_CARD, (rowIndex, columnIndex, callback) => {
-      recordInfor(
-        `received clickCard request from token is:${GAME_SYSTEM.getTokenBySocket(socket)}, rowIndex:${rowIndex} columnIndex:${columnIndex}`
-      );
-      GAME_SYSTEM.clickCard(socket, rowIndex, columnIndex);
-
-      if (callback != null) {
-        callback();
-      }
-    });
-
-    socket.on(REPLAY, callback => {
-      recordInfor(`received replay request from token is:${GAME_SYSTEM.getTokenBySocket(socket)}`);
-      GAME_SYSTEM.replay(socket);
-      callback();
-    });
-
-    socket.on(GAME_MODE_CHANGED, mode => {
-      recordInfor(`received gameModeChanged request from token:${socket.handshake.query.token}, new mode:${mode}`);
-      GAME_SYSTEM.changeGameMode(socket, mode);
-    });
+    subscribeGetData(socket);
+    subscribeClickCard(socket);
+    subscribeReplay(socket);
+    subscribeGameModeChange(socket);
   });
-}
-
-function startServer(port) {
-  APP.listen(port);
-  setSubscribeForSocketIO(IO);
   recordInfor(`app listen at:${port}`);
 }
 
+function subscribeGameModeChange(socket) {
+  socket.on(GAME_MODE_CHANGED, mode => {
+    recordInfor(`received gameModeChanged request from token:${socket.handshake.query.token}, new mode:${mode}`);
+    GAME_SYSTEM.changeGameMode(socket, mode);
+  });
+}
+
+function subscribeReplay(socket) {
+  socket.on(REPLAY, callback => {
+    recordInfor(`received replay request from token is:${GAME_SYSTEM.getTokenBySocket(socket)}`);
+    GAME_SYSTEM.replay(socket);
+    callback();
+  });
+}
+
+function subscribeClickCard(socket) {
+  socket.on(CLICK_CARD, (rowIndex, columnIndex, callback) => {
+    recordInfor(`received clickCard request from token is:${GAME_SYSTEM.getTokenBySocket(socket)}, rowIndex:${rowIndex} columnIndex:${columnIndex}`);
+    GAME_SYSTEM.clickCard(socket, rowIndex, columnIndex);
+    if (callback != null) {
+      callback();
+    }
+  });
+}
+
+function subscribeGetData(socket) {
+  socket.on(GET_DATA, () => {
+    const token = GAME_SYSTEM.getTokenBySocket(socket);
+    const gameDatas = GAME_SYSTEM.getGameDatasByToken(token);
+    recordInfor(`received getData request, request id: ${socket.id}, token is:${token}`);
+    GAME_SYSTEM.emitGameDatas(gameDatas);
+  });
+}
+
 function stopServer() {
-  APP.close();
+  IO.close(() => {
+    APP.close(() => {
+      recordInfor(`service stopped`);
+    });
+  });
 }
 
 module.exports = {
--- test/intergration_test/peace-mode.test.js
+++ test/intergration_test/peace-mode.test.js
@@ -57,144 +57,144 @@ describe('Peace Mode', function() {
     });
   });
 
-  // describe('Simple Combine Cards', function() {
-  //   it('should have one card in number map, card value should be 2, for combining one card(value: 1)', function(done) {
-  //     clientSocket.emit(CLICK_CARD, 0, 1, () => {
-  //       clientSocket.emit(CLICK_CARD, 0, 0, () => {
-  //         var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //         expectedGameDatas.addCard(0, 0, 2);
-  //         expectedGameDatas.score = 1;
+  describe('Simple Combine Cards', function() {
+    it('should have one card in number map, card value should be 2, for combining one card(value: 1)', function(done) {
+      clientSocket.emit(CLICK_CARD, 0, 1, () => {
+        clientSocket.emit(CLICK_CARD, 0, 0, () => {
+          var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+          expectedGameDatas.addCard(0, 0, 2);
+          expectedGameDatas.score = 1;
 
-  //         var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
-  //         var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
+          var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+          var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
 
-  //         expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
-  //         done();
-  //       });
-  //     });
-  //   });
+          expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
+          done();
+        });
+      });
+    });
 
-  //   it('should have one card in number map, card value should be 2, for combining two cards (value: 1)', function(done) {
-  //     clientSocket.emit(CLICK_CARD, 0, 0, () => {
-  //       clientSocket.emit(CLICK_CARD, 0, 2, () => {
-  //         clientSocket.emit(CLICK_CARD, 0, 1, () => {
-  //           var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //           expectedGameDatas.addCard(0, 1, 2);
-  //           expectedGameDatas.score = 2;
+    it('should have one card in number map, card value should be 2, for combining two cards (value: 1)', function(done) {
+      clientSocket.emit(CLICK_CARD, 0, 0, () => {
+        clientSocket.emit(CLICK_CARD, 0, 2, () => {
+          clientSocket.emit(CLICK_CARD, 0, 1, () => {
+            var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+            expectedGameDatas.addCard(0, 1, 2);
+            expectedGameDatas.score = 2;
 
-  //           var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
-  //           var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
+            var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+            var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
 
-  //           expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
-  //           done();
-  //         });
-  //       });
-  //     });
-  //   });
+            expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
+            done();
+          });
+        });
+      });
+    });
 
-  //   it('should have one card in number map, card value should be 3, score should be 3, for combining 2 rounds with cards(value: 1,2)', function(done) {
-  //     var mockedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //     mockedGameDatas.addCard(1, 0, 1).addCard(1, 1, 2);
-  //     var mockedResult = mockedGameDatas.getResult();
-  //     var mockedCandidateCards = [1, 1];
-  //     var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
-  //     gameDatas.numberCardsMap = mockedResult.numberCardsMap;
-  //     gameDatas.emptyCardsMap = mockedResult.emptyCardsMap;
-  //     gameDatas.playgroundCards = mockedResult.playgroundCards;
-  //     gameDatas.candidateCards = mockedCandidateCards;
+    it('should have one card in number map, card value should be 3, score should be 3, for combining 2 rounds with cards(value: 1,2)', function(done) {
+      var mockedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+      mockedGameDatas.addCard(1, 0, 1).addCard(1, 1, 2);
+      var mockedResult = mockedGameDatas.getResult();
+      var mockedCandidateCards = [1, 1];
+      var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+      gameDatas.numberCardsMap = mockedResult.numberCardsMap;
+      gameDatas.emptyCardsMap = mockedResult.emptyCardsMap;
+      gameDatas.playgroundCards = mockedResult.playgroundCards;
+      gameDatas.candidateCards = mockedCandidateCards;
 
-  //     clientSocket.emit(CLICK_CARD, 0, 0, () => {
-  //       var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //       expectedGameDatas.addCard(0, 0, 3);
-  //       expectedGameDatas.score = 3;
+      clientSocket.emit(CLICK_CARD, 0, 0, () => {
+        var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+        expectedGameDatas.addCard(0, 0, 3);
+        expectedGameDatas.score = 3;
 
-  //       var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
+        var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
 
-  //       expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
-  //       done();
-  //     });
-  //   });
+        expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
+        done();
+      });
+    });
 
-  //   it('should have one card in number map, card value should be 4, score should be 6, for combining 3 rounds with cards(value: 1,2,3)', function(done) {
-  //     var mockedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //     mockedGameDatas
-  //       .addCard(1, 0, 1)
-  //       .addCard(1, 1, 2)
-  //       .addCard(1, 2, 3);
-  //     var mockedResult = mockedGameDatas.getResult();
-  //     var mockedCandidateCards = [1, 1];
-  //     var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
-  //     gameDatas.numberCardsMap = mockedResult.numberCardsMap;
-  //     gameDatas.emptyCardsMap = mockedResult.emptyCardsMap;
-  //     gameDatas.playgroundCards = mockedResult.playgroundCards;
-  //     gameDatas.candidateCards = mockedCandidateCards;
+    it('should have one card in number map, card value should be 4, score should be 6, for combining 3 rounds with cards(value: 1,2,3)', function(done) {
+      var mockedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+      mockedGameDatas
+        .addCard(1, 0, 1)
+        .addCard(1, 1, 2)
+        .addCard(1, 2, 3);
+      var mockedResult = mockedGameDatas.getResult();
+      var mockedCandidateCards = [1, 1];
+      var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+      gameDatas.numberCardsMap = mockedResult.numberCardsMap;
+      gameDatas.emptyCardsMap = mockedResult.emptyCardsMap;
+      gameDatas.playgroundCards = mockedResult.playgroundCards;
+      gameDatas.candidateCards = mockedCandidateCards;
 
-  //     clientSocket.emit(CLICK_CARD, 0, 1, () => {
-  //       var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //       expectedGameDatas.addCard(0, 1, 4);
-  //       expectedGameDatas.score = 6;
+      clientSocket.emit(CLICK_CARD, 0, 1, () => {
+        var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+        expectedGameDatas.addCard(0, 1, 4);
+        expectedGameDatas.score = 6;
 
-  //       var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
+        var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
 
-  //       expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
-  //       done();
-  //     });
-  //   });
+        expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
+        done();
+      });
+    });
 
-  //   it('should have one card in number map, card value should be 5, score should be 10, for combining 4 rounds with cards(value: 1,2,3,4)', function(done) {
-  //     var mockedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //     mockedGameDatas
-  //       .addCard(1, 0, 1)
-  //       .addCard(1, 1, 2)
-  //       .addCard(1, 2, 3)
-  //       .addCard(0, 2, 4);
-  //     var mockedResult = mockedGameDatas.getResult();
-  //     var mockedCandidateCards = [1, 1];
-  //     var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
-  //     gameDatas.numberCardsMap = mockedResult.numberCardsMap;
-  //     gameDatas.emptyCardsMap = mockedResult.emptyCardsMap;
-  //     gameDatas.playgroundCards = mockedResult.playgroundCards;
-  //     gameDatas.candidateCards = mockedCandidateCards;
+    it('should have one card in number map, card value should be 5, score should be 10, for combining 4 rounds with cards(value: 1,2,3,4)', function(done) {
+      var mockedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+      mockedGameDatas
+        .addCard(1, 0, 1)
+        .addCard(1, 1, 2)
+        .addCard(1, 2, 3)
+        .addCard(0, 2, 4);
+      var mockedResult = mockedGameDatas.getResult();
+      var mockedCandidateCards = [1, 1];
+      var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+      gameDatas.numberCardsMap = mockedResult.numberCardsMap;
+      gameDatas.emptyCardsMap = mockedResult.emptyCardsMap;
+      gameDatas.playgroundCards = mockedResult.playgroundCards;
+      gameDatas.candidateCards = mockedCandidateCards;
 
-  //     clientSocket.emit(CLICK_CARD, 0, 1, () => {
-  //       var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //       expectedGameDatas.addCard(0, 1, 5);
-  //       expectedGameDatas.score = 10;
+      clientSocket.emit(CLICK_CARD, 0, 1, () => {
+        var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+        expectedGameDatas.addCard(0, 1, 5);
+        expectedGameDatas.score = 10;
 
-  //       var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
+        var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
 
-  //       expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
-  //       done();
-  //     });
-  //   });
+        expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
+        done();
+      });
+    });
 
-  //   it('should have one card in number map, card value should be 7, score should be 21, for combining 6 rounds with cards(value: 1,2,3,4,5,6)', function(done) {
-  //     var mockedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //     mockedGameDatas
-  //       .addCard(0, 0, 1)
-  //       .addCard(0, 1, 2)
-  //       .addCard(0, 2, 3)
-  //       .addCard(1, 0, 4)
-  //       .addCard(1, 2, 5)
-  //       .addCard(2, 0, 6);
-  //     var mockedResult = mockedGameDatas.getResult();
-  //     var mockedCandidateCards = [1, 1];
-  //     var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
-  //     gameDatas.numberCardsMap = mockedResult.numberCardsMap;
-  //     gameDatas.emptyCardsMap = mockedResult.emptyCardsMap;
-  //     gameDatas.playgroundCards = mockedResult.playgroundCards;
-  //     gameDatas.candidateCards = mockedCandidateCards;
+    it('should have one card in number map, card value should be 7, score should be 21, for combining 6 rounds with cards(value: 1,2,3,4,5,6)', function(done) {
+      var mockedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+      mockedGameDatas
+        .addCard(0, 0, 1)
+        .addCard(0, 1, 2)
+        .addCard(0, 2, 3)
+        .addCard(1, 0, 4)
+        .addCard(1, 2, 5)
+        .addCard(2, 0, 6);
+      var mockedResult = mockedGameDatas.getResult();
+      var mockedCandidateCards = [1, 1];
+      var gameDatas = GAME_SYSTEM.getGameDatasByToken(TOKEN);
+      gameDatas.numberCardsMap = mockedResult.numberCardsMap;
+      gameDatas.emptyCardsMap = mockedResult.emptyCardsMap;
+      gameDatas.playgroundCards = mockedResult.playgroundCards;
+      gameDatas.candidateCards = mockedCandidateCards;
 
-  //     clientSocket.emit(CLICK_CARD, 1, 1, () => {
-  //       var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
-  //       expectedGameDatas.addCard(1, 1, 7);
-  //       expectedGameDatas.score = 21;
+      clientSocket.emit(CLICK_CARD, 1, 1, () => {
+        var expectedGameDatas = TEST_UTIL.emptyMockedGameDatas();
+        expectedGameDatas.addCard(1, 1, 7);
+        expectedGameDatas.score = 21;
 
-  //       var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
+        var resultGameDatas = TEST_UTIL.getResultGameDatas(gameDatas);
 
-  //       expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
-  //       done();
-  //     });
-  //   });
-  // });
+        expectedGameDatas.getResult().should.be.deepEqual(resultGameDatas);
+        done();
+      });
+    });
+  });
 });
